b0ef9a95172ffc900c4a8a998453c44ec9f0935c
Amy Troppe <cmephro@aol.com>
2020-08-06 16:48:16 -0500

    Configure Angular Elements

--- projects/elements-app/src/app/app.module.ts
+++ projects/elements-app/src/app/app.module.ts
@@ -1,18 +1,20 @@
+import { Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { UiLibraryComponent, UiLibraryModule } from 'projects/ui-library/src/public-api';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [],
+  imports: [BrowserModule, UiLibraryModule],
   providers: [],
-  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const element = createCustomElement(UiLibraryComponent, { injector: this.injector });
+    customElements.define('lib-components', element);
+  }
+}
+
+// reference: https://medium.com/swlh/angular-elements-create-a-component-library-for-angular-and-the-web-8f7986a82999#:~:text=Currently%2C%20Angular%20Elements%20only%20supports,output%20them%20as%20Custom%20Elements.
