85e9b1f9dd2799fcb034c151f2ea2d27b4e73ed9
Amy Troppe <cmephro@aol.com>
2020-08-11 13:36:53 -0500

    add angular material navigation schematic to header library

--- projects/components-library/header/src/lib/header.module.ts
+++ projects/components-library/header/src/lib/header.module.ts
@@ -1,13 +1,27 @@
+import { LayoutModule } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { HeaderComponent } from './header.component';
 import { NavComponent } from './nav/nav.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
-  declarations: [HeaderComponent, NavComponent],
-  imports: [MatToolbarModule, MatSliderModule, MatIconModule],
+  declarations: [HeaderComponent, NavComponent, NavigationComponent],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatSliderModule,
+    MatIconModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+  ],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
